1596b9fbdede437a3657b5e71cce8288ef61d3ef
Evaniee <62286912+Evaniee@users.noreply.github.com>
2024-06-27 09:53:30 +0100

    Added Basic Files
    Files taken and adapted from Matrix TSL's resources Dance.py project.

---
+++ Program.py
@@ -0,0 +1,9 @@
+import FA
+fa = FA.Create()
+fa.ComOpen()        # TODO: Put your COM port number in the ()s
+try:
+    pass            # TODO: Replace with your code
+except:
+    pass            # TODO: Implement Error Handling
+finally:
+    fa.ComClose()
---
+++ readme.txt
@@ -0,0 +1,3 @@
+When using Python, please ensure the "FA.py" library file is in the same directory as your Python program.
+
+An installation for this Python library file will be available later.
